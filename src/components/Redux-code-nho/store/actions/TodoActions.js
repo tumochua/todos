97b2ee9408943636292964cb18db47aa93030265
nhan trach <tumochua14012003@gmail.com>
2022-07-11 20:54:38 +0700

    push code todo

--- src/components/Redux-code-nho/store/actions/TodoActions.js
+++
@@ -1,3 +0,0 @@
-export const markComplete = (id) => (dispatch) => {
-  dispatch({});
-};
--- src/components/Redux-code-nho/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// gộp tất cả các reducers => nếu có nhiều reducers thì rất tiện lợi
-import { combineReducers } from "redux";
-import todoReducers from "../reducers/todoReducer";
-
-const rootReducers = combineReducers({
-  myTodos: todoReducers,
-});
-export default rootReducers;
